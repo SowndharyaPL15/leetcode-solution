04a5f002734636a59c6707fed36748b032b02617
SowndharyaPL15 <plsowndharya@gmail.com>
2026-05-29 13:57:56 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (86.90%) - LeetSync

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minElement(int[] nums) {
+        int a=37;
+        for(int num:nums){
+            int d=0;
+            while(num>0){
+                d+=num%10;
+                num/=10;
+            }
+            a=Math.min(a,d);
+        }
+        return a;
+    }
+}
