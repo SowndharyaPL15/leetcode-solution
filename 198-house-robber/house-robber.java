cf456b4a9eac5ab78749c92865518fcda669da6c
SowndharyaPL15 <plsowndharya@gmail.com>
2026-06-16 12:20:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (36.56%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int rob(int[] nums) {
+       int p1=0,p2=0;
+       for(int num : nums){
+            int temp = Math.max(p1, p2 + num);
+            p2 = p1;
+            p1 = temp;
+        }
+        return p1;
+    }
+}
