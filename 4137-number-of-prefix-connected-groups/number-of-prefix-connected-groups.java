305c72964633b4d55574e8e3ac8be3176aa8d0ee
SowndharyaPL15 <plsowndharya@gmail.com>
2026-06-24 11:04:46 +0530

    Time: 18 ms (97.53%) | Memory: 48.7 MB (87.04%) - LeetSync

---
+++ 4137-number-of-prefix-connected-groups/number-of-prefix-connected-groups.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int prefixConnected(String[] words, int k) {
+        Set<String>r=new HashSet<>();
+        Set<String>s=new HashSet<>();
+        for(String w:words){
+            if(w.length()<k){
+                continue;
+            }
+            String p = w.substring(0,k);
+            if(!s.add(p)){
+                r.add(p);
+            }
+        }
+        return r.size();
+    }
+}
+
